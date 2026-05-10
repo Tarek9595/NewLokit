906f826eb06f71cca40a1a30e47e9f61856f4850
TekaSayed <160263924+TekaSayed@users.noreply.github.com>
2026-05-10 03:40:39 +0300

    adasd

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
     const newUser = {
       firstName: "Usernameee",
       lastName: "Developer",
-      email: `userr963741@test.com`,
+      email: `userr20262027@test.com`,
       password: "password123",
       phone: "01012345678",
     };
